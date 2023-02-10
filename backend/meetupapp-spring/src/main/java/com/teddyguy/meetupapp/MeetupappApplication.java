dafc66dea41c48b91d196fc515433dc9fe59adcd
TeddyGuy <89847992+TeddyGuy@users.noreply.github.com>
2023-02-10 13:47:58 -0500

    first commit

---
+++ backend/meetupapp-spring/src/main/java/com/teddyguy/meetupapp/MeetupappApplication.java
@@ -0,0 +1,13 @@
+package com.teddyguy.meetupapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetupappApplication.class, args);
+	}
+
+}
---
+++ backend/meetupapp-spring/src/test/java/com/teddyguy/meetupapp/MeetupappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teddyguy.meetupapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetupappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
